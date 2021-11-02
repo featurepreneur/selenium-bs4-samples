5257e457fea8ad41f331af373791ad4186770c99
aravindkv27 <aravind27032002@gmail.com>
2021-11-02 21:55:27 +0530

    Prize and parking

--- hosted.py
+++ hosted.py
@@ -1,29 +1,52 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
+from selenium.common.exceptions import NoSuchElementException
+from  bs4 import BeautifulSoup
+import requests
 
 PATH="/home/aravind/Featurepreneur/chromedriver"
 
 driver=webdriver.Chrome(PATH)
 
-driver.get("https://spaceishare.com/listing/parking-space/ontario/toronto/secure-underground-parking-13030")
+link1="https://spaceishare.com/listing/parking-space/ontario/toronto/parking-at-finch-and-younge-area-21184"
+link=driver.get(link1)
 
-hosted_by=driver.find_element_by_xpath('//*[@id="contact_form"]/div/div/p/a').text
-print(hosted_by)
+# hosted_by=driver.find_element_by_xpath('//*[@id="contact_form"]/div/div/p/a').text
+# print(hosted_by)
 
-type_of_space=driver.find_element_by_xpath('/html/body/div[2]/div/div[2]/div[1]/div[4]/div/p[4]').text
-print(type_of_space)
+# type_of_space=driver.find_element_by_xpath('/html/body/div[2]/div/div[2]/div[1]/div[4]/div/p[4]').text
+# print(type_of_space)
 
-
+# sp_det=driver.find_elements_by_css_selector("body > div.upload_page.desktop-view.showlisting > div > div:nth-child(2) > div.col-md-6.float-left.position-relative.bg-white.pad-none.content-section > div.row.mr-30.row-pad > div > p:nth-child(6)").text
+# print(sp_det)
+# count=1
 space_fea_list=[]
-space_fea=driver.find_elements_by_xpath("//p[@class='p-font-size']")
+
+# while True:
+#     try:
+#         space_fea=driver.find_elements_by_xpath(f"//p[@class='p-font-size'][{count}]")
 
 
-for text in space_fea:
+#         for text in space_fea:
 
-    text_of_sf=text.text
-    space_fea.append(text_of_sf)
+#             text_of_sf=text.text
+            
+#             space_fea.append(text_of_sf)
 
-listToStr = ' '.join(map(str, space_fea_list))
-print(listToStr)
+#     except NoSuchElementException:
+#         break
+# print(spa)
+# listToStr = ' '.join(map(str, space_fea_list))
+# print(listToStr)
+
+res=requests.get(link1)
+html_page=res.content
+soup=BeautifulSoup(html_page,'html.parser')
+mydivs = soup.find("div", {"class": "row mr-30 row-pad"})
+for t in mydivs.find_all("p", {"class": "p-font-size"}):
+    a=t.text.strip()
+    space_fea_list.append(a)
+
+print(space_fea_list) 
 driver.quit()
--- spaceishare/test.py
+++ spaceishare/test.py
@@ -3,10 +3,11 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
 import pandas as pd
 import time
 
-PATH="/home/chaaya/softwares/chromedriver"
+PATH="/home/aravind/Featurepreneur/chromedriver"
 
 driver=webdriver.Chrome(PATH)
 
@@ -21,19 +22,44 @@ pwd = driver.find_element_by_name("pwd")
 pwd.send_keys('tactlabs123')
 driver.find_element_by_name("login").click()
 
-driver.get("https://spaceishare.com/profile/myProfile/1")
+driver.get("https://spaceishare.com/profile/myProfile/1051")
 
 #  scroll = driver.find_elements_by_css_selector('#load_data_message > h3')
 wait = WebDriverWait(driver, 10) 
-
+prize=[]
+parking=[]
 while True: 
-    try:
-        wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="load_data_message"]/h3')))
-        break
-    except:    
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        time.sleep(3)
+    
+        prize_amount=1
+        while True:
+            try:
+                amount=driver.find_element_by_css_selector(f'#product-id > div:nth-child({prize_amount}) > div > div > div.profile-prize').text
+                prize.append(amount)
+                prize_amount+=1
+            except NoSuchElementException:
+                break
+        add_val=1
+        while True:
+            try:
+                add=driver.find_element_by_xpath(f'//*[@id="product-id"]/div[{add_val}]/div/div/div[2]/a[1]/h5').text
+                parking.append(add)
+                add_val+=1
+            except NoSuchElementException:
+                break
+        # profile_prize=driver.find_element_by_css_selector("#product-id > div:nth-child(1) > div > div > div.profile-prize").text
+           
         
+        try:
 
+            wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="load_data_message"]/h3')))
+            break
+            
+        except:    
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(3)
+                
+            
 
+print(prize)
+print(parking)  
 print("done")
